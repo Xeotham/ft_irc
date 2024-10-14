1c8ccf53e6753655dda70aaf88ab8694f5f97388
mathaouas <mat.haouas@gmail.com>
2024-10-14 07:38:26 +0200

    Starting to make Channels

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,4 +1,5 @@
 #include "Client.hpp"
+// #include "Channel.hpp
 
 Client::Client()
 {
@@ -18,11 +19,15 @@ void	Client::setIpAdd(std::string ip)
 	this->_ipAdd = ip;
 }
 
-void Client::setChannel(const Channel &new_channel) {
-	if (&this->getChannel() != &new_channel)
-		this->_current_channel = new_channel;
+void Client::setChannel(Channel &new_channel) {
+	if (this->getChannel() != &new_channel)
+		this->_current_channel = &new_channel;
 	else
 		throw (std::invalid_argument("User is already in the channel."));
+}
+
+Channel *Client::getChannel() const {
+	return (this->_current_channel);
 }
 
 int		Client::getFd() const
